9598c3992affc26794002648a447f53244229eb1
zer0 <internetghost0@protonmail.com>
2023-07-12 17:49:56 +0000

    init

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -xe
+cc -Wall -Wextra -o main ./main.c
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+int main(void) {
+    printf("Hello world!\n");
+    return 0;
+}
